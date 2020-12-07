138e8c1b29f33be3c3bb75c401202d5cb181a60e
Mac009 <mac009@iMac-Mac009.local>
2020-12-07 14:06:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication22/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication22;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstancestate) {
+        super.onCreate(savedInstancestate);
+        setContentView(R.layout.sign_in);
+    }
+    public
+}
---
+++ app/src/main/java/com/example/myapplication22/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 5*600 );
+    }
+
+}
